50b24c5ab2c430243d39a28ff2a200e2fdb31834
SOVIDIU18 <simionescuo@yahoo.com>
2024-04-10 12:56:58 +0300

    nou-nout

--- bazadedate.js
+++ bazadedate.js
@@ -1,10 +1,28 @@
+
+
 function fct1() {
     let pretmin = document.getElementById("fromInput").value
     let pretmax = document.getElementById("toInput").value
+    let ordonare = document.getElementById("ordonare_suma").selectedIndex;
+    let sorting;
+    switch (ordonare) {
+        case 1:
+            sorting = '+nume'
+            break;
+        case 2:
+            sorting = '+pret'
+            break;
+        case 3:
+            sorting = '-pret'
+            break;
+        default:
+            sorting = '';
+    }
     let cerere = axios.get(`http://127.0.0.1:8090/api/collections/centrale/records`, {
         params: {
             filter: `(pret>=${pretmin} && pret<=${pretmax})`,
-            perPage: 100
+            perPage: 100,
+            sort: sorting
         }
     }).then(function (response) {
         // console.log(response.data)
@@ -21,7 +39,7 @@ function fct1() {
             centrala.className = "element"
             let nume1 = document.createElement("a");
             nume1.className = "text"
-            nume1.href = "/produs.html?id="+data["id"]
+            nume1.href = "/produs.html?id=" + data["id"]
             let pret1 = document.createElement("p");
             pret1.className = "pret"
             let stoc1 = document.createElement("input");
@@ -30,11 +48,12 @@ function fct1() {
             poza.className = "imagine"
             poza.src = "date site\\Site Contcare\\Produse\\Beretta\\Power Max 65P (sistem)\\Power_Max_-_Imagine-removebg-preview.png"
             nume1.textContent += nume;
-            pret1.textContent += pret + " RON " ;
+            pret1.textContent += pret + " RON ";
             stoc1.checked = (stoc == "true");
-            
 
-            
+
+
+
             centrala.appendChild(poza);
             // centrala.appendChild(stoc1);
             maindiv.appendChild(centrala)
@@ -50,6 +69,7 @@ document.addEventListener("DOMContentLoaded", function () {
     let pretmax = document.getElementById("toInput").addEventListener('change', schimba)
     let pretmin1 = document.getElementById("fromSlider").addEventListener('change', schimba)
     let pretmax1 = document.getElementById("toSlider").addEventListener('change', schimba)
+    let sortare = document.getElementById("ordonare_suma").addEventListener('change', schimba)
     fct1();
 });
 
--- produs.js
+++ produs.js
@@ -9,6 +9,9 @@ function fcn2(){
         }
     }).then(function (response) {
         console.log(response)
+        let maindiv = document.getElementById("maindiv");
+        let pozadiv = document.getElementById("poza");
+        let titludiv = document.getElementById("titlu");
         let data = response.data["items"][0]
             let nume = data["nume"]
             let pret = data["pret"]
@@ -17,9 +20,8 @@ function fcn2(){
             console.log(`${nume} ${pret} ${stoc}`)
             let centrala = document.createElement("div")
             centrala.className = "element"
-            let nume1 = document.createElement("a");
+            let nume1 = document.createElement("p");
             nume1.className = "text"
-            nume1.href = "/produs.html?id="+data["id"]
             let pret1 = document.createElement("p");
             pret1.className = "pret"
             let stoc1 = document.createElement("input");
@@ -33,13 +35,23 @@ function fcn2(){
             let descriere = document.createElement("p");
             descriere.textContent = data["descriere"]
             descriere.className = "descriere"
+            let textdescriere = document.createElement("p")
+            textdescriere.className = "textdescriere"
+            textdescriere.textContent = "Descriere"
+            let buton = document.createElement("input")
+            buton.type = "button"
+            buton.value = "Adaugă în coș"
+            buton.className="buton"
+            // buton.onclick= 
 
-            
-            centrala.appendChild(poza);
-            // centrala.appendChild(stoc1);
             maindiv.appendChild(centrala)
-            centrala.appendChild(nume1);
-            centrala.appendChild(pret1);
+            pozadiv.appendChild(poza);
+            // centrala.appendChild(stoc1);
+           
+            titludiv.appendChild(nume1);
+            titludiv.appendChild(pret1);
+            titludiv.appendChild(buton)
+            centrala.appendChild(textdescriere);
             centrala.appendChild(descriere);
 })
 }
